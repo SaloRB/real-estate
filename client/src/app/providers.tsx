0b927062424c49c9dc495061ab7cf3b3c74a58ea
Salomon Romano <salo_romano@yahoo.com>
2025-02-28 11:56:18 -0600

    add AWS Amplify authentication with UI components and API integration

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,9 +1,19 @@
 'use client'
 
+import { Authenticator } from '@aws-amplify/ui-react'
+
+import Auth from '@/providers/AuthProvider'
 import StoreProvider from '@/state/redux'
 
+
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <StoreProvider>{children}</StoreProvider>
+  return (
+    <StoreProvider>
+      <Authenticator.Provider>
+        <Auth>{children}</Auth>
+      </Authenticator.Provider>
+    </StoreProvider>
+  )
 }
 
 export default Providers
