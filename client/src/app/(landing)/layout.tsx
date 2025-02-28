4a7f23244204cf21bbc5f5ac813cad9a04efe56f
Salomon Romano <salo_romano@yahoo.com>
2025-02-28 13:49:34 -0600

    enhance navbar functionality; integrate user role-based actions and sign-out feature

--- client/src/app/(landing)/layout.tsx
+++ client/src/app/(landing)/layout.tsx
@@ -2,12 +2,8 @@
 
 import Navbar from '@/components/Navbar'
 import { NAVBAR_HEIGHT } from '@/lib/constants'
-import { useGetAuthUserQuery } from '@/state/api'
 
 const NonDashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  const { data: authUser } = useGetAuthUserQuery()
-  console.log(`authUser: `, authUser)
-
   return (
     <div className="h-full w-full">
       <Navbar />
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,38 @@
-import { NAVBAR_HEIGHT } from '@/lib/constants'
+import { signOut } from 'aws-amplify/auth'
+import { BellIcon, MessageCircleIcon, PlusIcon, SearchIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
+
+import { NAVBAR_HEIGHT } from '@/lib/constants'
+import { useGetAuthUserQuery } from '@/state/api'
+
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import { SidebarTrigger } from './ui/sidebar'
 
 const Navbar = () => {
+  const { data: authUser } = useGetAuthUserQuery()
+  const router = useRouter()
+  const pathname = usePathname()
+
+  const isDashboardPage =
+    pathname.includes('/managers') || pathname.includes('/tenants')
+
+  const userRole = authUser?.userRole.toLowerCase()
+
+  const handleSignOut = async () => {
+    await signOut()
+    window.location.href = '/'
+  }
+
   return (
     <div
       className="fixed top-0 left-0 w-full z-50 shadow-xl"
@@ -11,6 +40,11 @@ const Navbar = () => {
     >
       <div className="flex justify-between items-center w-full py-3 px-8 bg-primary-700 text-white">
         <div className="flex items-center gap-4 md:gap-6">
+          {isDashboardPage && (
+            <div className="md:hidden">
+              <SidebarTrigger />
+            </div>
+          )}
           <Link
             href="/"
             className="cursor-pointer hover:!text-primary-300"
@@ -32,27 +66,113 @@ const Navbar = () => {
               </div>
             </div>
           </Link>
-        </div>
-        <p className="text-primary-200 hidden md:block">
-          Discover your perfect rental apartment with our advanced search
-        </p>
-        <div className="flex items-center gap-5">
-          <Link href="/sign-in">
-            <Button
-              variant="outline"
-              className="text-white border-white bg-transparent hover:bg-white hover:text-primary-700 rounded-lg"
-            >
-              Sign In
-            </Button>
-          </Link>
-          <Link href="/sign-up">
+          {isDashboardPage && authUser && (
             <Button
               variant="secondary"
-              className="text-white bg-secondary-600 hover:bg-white hover:text-primary-700 rounded-lg"
+              className="md:ml-4 bg-primary-50 text-primary-700 hover:bg-secondary-500 hover:text-primary-50"
+              onClick={() =>
+                router.push(
+                  userRole === 'manager' ? '/managers/newproperty' : '/search'
+                )
+              }
             >
-              Sign Up
+              {userRole === 'manager' ? (
+                <>
+                  <PlusIcon className="size-4" />
+                  <span className="hidden md:block ml-2">Add New Property</span>
+                </>
+              ) : (
+                <>
+                  <SearchIcon className="size-4" />
+                  <span className="hidden md:block ml-2">
+                    Search Properties
+                  </span>
+                </>
+              )}
             </Button>
-          </Link>
+          )}
+        </div>
+        {!isDashboardPage && (
+          <p className="text-primary-200 hidden md:block">
+            Discover your perfect rental apartment with our advanced search
+          </p>
+        )}
+        <div className="flex items-center gap-5">
+          {authUser ? (
+            <>
+              <div className="relative hidden md:block">
+                <MessageCircleIcon className="size-6 cursor-pointer text-primary-200 hover:text-primary-400" />
+                <span className="absolute top-0 right-0 size-2 bg-secondary-700 rounded-full" />
+              </div>
+              <div className="relative hidden md:block">
+                <BellIcon className="size-6 cursor-pointer text-primary-200 hover:text-primary-400" />
+                <span className="absolute top-0 right-0 size-2 bg-secondary-700 rounded-full" />
+              </div>
+
+              <DropdownMenu>
+                <DropdownMenuTrigger className="flex items-center gap-2 focus:outline-none">
+                  <Avatar>
+                    <AvatarImage src={authUser.userInfo?.image} />
+                    <AvatarFallback className="bg-primary-600">
+                      {userRole[0].toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                  <p className="text-primary-200 hidden md:block">
+                    {authUser.userInfo?.name}
+                  </p>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="bg-white text-primary-700">
+                  <DropdownMenuItem
+                    className="cursor-pointer hover:!bg-primary-700 hover:!text-primary-100 font-bold"
+                    onClick={() =>
+                      router.push(
+                        userRole === 'manager'
+                          ? '/managers/properties'
+                          : '/tenants/favorites',
+                        { scroll: false }
+                      )
+                    }
+                  >
+                    Dashboard
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator className="bg-primary-200" />
+                  <DropdownMenuItem
+                    className="cursor-pointer hover:!bg-primary-700 hover:!text-primary-100"
+                    onClick={() =>
+                      router.push(`/${userRole}s/settings`, { scroll: false })
+                    }
+                  >
+                    Settings
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="cursor-pointer hover:!bg-primary-700 hover:!text-primary-100"
+                    onClick={handleSignOut}
+                  >
+                    Sign Out
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </>
+          ) : (
+            <>
+              <Link href="/sign-in">
+                <Button
+                  variant="outline"
+                  className="text-white border-white bg-transparent hover:bg-white hover:text-primary-700 rounded-lg"
+                >
+                  Sign In
+                </Button>
+              </Link>
+              <Link href="/sign-up">
+                <Button
+                  variant="secondary"
+                  className="text-white bg-secondary-600 hover:bg-white hover:text-primary-700 rounded-lg"
+                >
+                  Sign Up
+                </Button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </div>
